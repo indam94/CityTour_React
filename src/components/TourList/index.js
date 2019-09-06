b372803cfa40102ab61e43147401a9ababa9400e
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-06 16:20:44 -0700

    TourList Component

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import Tour from '../Tour';
+import './TourList.scss';
+import {tourData} from '../../tourData';
 
 export default class TourList extends Component {
+    state={
+        tours: tourData
+    };
+
+    removeTourHandler = id =>{
+        console.log(id);
+    };
+
     render() {
+        // console.log(this.state.tours);
+
+        const {tours} = this.state;
+
         return (
-            <div>
-                Hello From TourList
-                <Tour/>
-            </div>
+            <section className = 'tourlist'>
+                {
+                    tours.map(tour => (<Tour 
+                                            key={tour.id} 
+                                            tour={tour}
+                                            removeTour={this.removeTourHandler}
+                                        >
+                                            
+                                        </Tour>))
+                }
+            </section>
         )
     }
 }
