c6c431e88fb0f29d729c38ded6d8844f3846fa82
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-06 16:07:17 -0700

    Main Project Structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar'
 import "@fortawesome/fontawesome-free/css/all.min.css"
+import TourList from './components/TourList';
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar></Navbar>
+      <Navbar />
+      <TourList/ >
     </React.Fragment>
   );
 }
