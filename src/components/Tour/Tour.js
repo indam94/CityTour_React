331dad4fb132d4411133bad1507927120cdec9a2
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-06 17:29:18 -0700

    Tour Info Toggle

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,6 +3,17 @@ import './Tour.scss'
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
 export default class Tour extends Component {
+
+    state = {
+        showInfo: false
+    };
+
+    handleInfo = () => {
+        this.setState({
+            showInfo:!this.state.showInfo
+        });
+    }
+
     render() {
 
         console.log(this.props);
@@ -21,11 +32,12 @@ export default class Tour extends Component {
                     <h3>{city}</h3>
                     <h4>{name}</h4>
                     <h5>info{" "}
-                        <span>
+                        <span onClick={this.handleInfo}>
                             <i className="fas fa-caret-square-down"/>
                         </span>
                     </h5>
-                    <p>{info}</p>
+                    {this.state.showInfo && <p>{info}</p>}
+                    
                 </div>
             </article>
             
