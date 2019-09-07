11e91eb32b930c2120fff8f0327817355aae3ef7
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-06 17:25:26 -0700

    Tour Component Styling

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -20,11 +20,12 @@ export default class Tour extends Component {
                 <div className="tour-info">
                     <h3>{city}</h3>
                     <h4>{name}</h4>
-                    <h5>info 
+                    <h5>info{" "}
                         <span>
                             <i className="fas fa-caret-square-down"/>
                         </span>
                     </h5>
+                    <p>{info}</p>
                 </div>
             </article>
             
